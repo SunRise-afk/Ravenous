38e7c8df9b6796085d0a1552be2efbd8cb3c9e71
Illia Ilin <sunrise.th13@gmail.com>
2020-12-07 11:45:55 +0200

    SearchBar and Reastaurants markup was added

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Business } from "../Business/Business";
+import "./BusinessList.css";
+
+export const BusinessList = () => {
+  return (
+    <div className="BusinessList">
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+      <Business></Business>
+    </div>
+  );
+};
